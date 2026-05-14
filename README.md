6c3ed554fdc7e6f5f1fcf56d243c8f09bebc9e3f
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-05-14 21:02:23 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1111
+![HTML Validated](https://img.shields.io/w3c-validation/html?targetUrl=https%3A%2F%2FGennadiyTsekhmistro.github.io%2Fgoit-markup-hw-03%2F)
+![GitHub Pages](https://img.shields.io/github/deployments/GennadiyTsekhmistro/goit-markup-hw-03/github-pages)
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./css/styles.css";
+import "./js/header.js";
+import "./js/hero.js";
+
+document.querySelector('#app').innerHTML = `
+  <div class="hero">
+    <img src="/illustration.svg" class="illustration" />
+  </div>
+`;
+
+document.querySelector('#app').innerHTML = `
+  <div class="logotypes">
+    <img src="/logotypes.svg" class="logotypes" />
+  </div>
+`;
