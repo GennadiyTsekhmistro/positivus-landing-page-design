823a0d1d6fa212f220a7ca2773a42dba7ab4d5d6
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-05-18 19:46:56 +0300

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@ import "./css/styles.css";
 import "./js/header.js";
 import "./js/hero.js";
 
+// document.querySelector('#app').innerHTML = `
+//   <div class="hero">
+//     <img src="/illustration.svg" class="illustration" />
+//   </div>
+// `;
+
+// document.querySelector('#app').innerHTML = `
+//   <div class="logotypes">
+//     <img src="/logotypes.svg" class="logotypes" />
+//   </div>
+// `;
+
 document.querySelector('#app').innerHTML = `
   <div class="hero">
     <img src="/illustration.svg" class="illustration" />
   </div>
-`;
 
-document.querySelector('#app').innerHTML = `
   <div class="logotypes">
-    <img src="/logotypes.svg" class="logotypes" />
+    <img src="/logotypes.svg" class="logotypes-img" />
   </div>
 `;
